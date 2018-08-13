b5a0007943d355d98c471945ce5f3ab96c72a27a
yolstatrisch <y00ra@naver.com>
2018-08-14 01:46:44 +0800

    Added Files

---
+++ e100.py
@@ -0,0 +1,15 @@
+def run(n, m):
+    t = m + 100
+    n = (n * t / m if m != 0 else n) + 1
+    m = t
+
+    return n, m
+
+if __name__ == '__main__':
+    epoch, progress, total = 0, 0, 0;
+
+    while total == 0 or progress / total < 1:
+        progress, total = run(progress, total)
+        epoch += 1
+
+    print('Final Epoch: {}', epoch)
